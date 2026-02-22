886f2a31cdce7d3cde115a68040c2058a9329077
Josiah Luke <Josiah Luke@LAPTOP-ITO8MURB>
2026-02-23 02:58:34 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+function sendMail() {
+    var params ={
+        name:document.getElementById("name").value,
+        email: document.getElementById("email").value,
+        message: document.getElementById("message").value,
+    };
+
+    const serviceID = "service_8vuhkqx";
+    const templateID = "template_k9n7p9i";
+
+    emailjs
+        .send(serviceID, templateID, params)
+        .then((res) =>{
+            document.getElementById("name").value = "";
+            document.getElementById("email").value = "";
+            document.getElementById("message").value = "";
+            console.log(res);
+            alert("Your message sent successfully");
+        })
+        .catch((err) => console.log(err));
+}
